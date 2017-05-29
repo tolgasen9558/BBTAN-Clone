177625ff67b4df2a509533ffe20e8148d88c1d3e
Tolga Şen <tolgasen9558@gmail.com>
2017-05-30 01:30:20 +0300

    Added LineRenderer to line breaker powerups to visualize horizontal or vertical hit

--- Assets/Power-Ups/Scripts/BreakHorizontalPowerup.cs
+++ Assets/Power-Ups/Scripts/BreakHorizontalPowerup.cs
@@ -5,20 +5,32 @@ using UnityEngine;
 public class BreakHorizontalPowerup : MonoBehaviour {
 
     private BlockController blockController;
+    private LineRenderer lineRenderer;
+    private bool flashTrigger = false;
 
 	// Use this for initialization
 	void Start () {
-        blockController = FindObjectOfType<BlockController>();		
+        blockController = FindObjectOfType<BlockController>();
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (flashTrigger) {
+            lineRenderer.enabled = true;
+            flashTrigger = false;
+        }
+        else {
+            lineRenderer.enabled = false;
+        }
+
 	}
 
     void OnTriggerEnter2D(Collider2D other) {
         Ball ball = other.GetComponent<Ball>();
         if(ball != null) {
+            flashTrigger = true;
             blockController.BreakHorizontalLine(transform.position.y);
         }
     }
--- Assets/Power-Ups/Scripts/BreakVerticalPowerup.cs
+++ Assets/Power-Ups/Scripts/BreakVerticalPowerup.cs
@@ -5,22 +5,40 @@ using UnityEngine;
 public class BreakVerticalPowerup : MonoBehaviour {
 
     private BlockController blockController;
+    private LineRenderer lineRenderer;
 
+    private bool flashTrigger = false;
 
     // Use this for initialization
     void Start () {
         blockController = FindObjectOfType<BlockController>();
+        lineRenderer = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update () {
-		
-	}
+        if (flashTrigger) {
+            lineRenderer.enabled = true;
+            flashTrigger = false;
+        }
+        else {
+            lineRenderer.enabled = false;
+        }
+    }
 
     void OnTriggerEnter2D(Collider2D other) {
         Ball ball = other.GetComponent<Ball>();
         if (ball != null) {
+            ShowLineRenderer();
             blockController.BreakVerticalLine(transform.position.x);
         }
+    }
+
+    void ShowLineRenderer() {
+        Vector3 topPoint = new Vector3(transform.position.x, 5, 0);
+        Vector3 bottomPoint = new Vector3(transform.position.x, -4, 0);
+        lineRenderer.SetPosition(0, topPoint);
+        lineRenderer.SetPosition(1, bottomPoint);
+        flashTrigger = true;
     }
 }
