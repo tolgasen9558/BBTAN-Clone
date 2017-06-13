7f0c2d868599814ee6ae4d4a0e046356c0962287
Tolga Şen <tolgasen9558@gmail.com>
2017-06-14 00:25:44 +0300

    Created Main Screen

---
+++ Assets/Starter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Starter : MonoBehaviour {
+
+	
+    public void StartTheGame() {
+        print("here");
+    }
+}
